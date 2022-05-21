0b4a0b2f1cdb2be90d2ad86762ceb76b2ab72052
Olivia McKee <olivia.mckee97@gmail.com>
2022-05-21 14:24:33 -0500

    display a question and answers

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,75 @@
 var startButtonEl = document.getElementById("start-button");
 var timerEl = document.getElementById("timer");
-var time = 60;
-function startTimer() {
+var time = 10;
+var questionsEl = document.getElementById("question");
+var startGameTextEl = document.getElementById("start-game-text");
+var answerChoicesEl = document.querySelector(".answer-choices");
+var questionTextEl = document.getElementById("question-text");
+var questionIndex = 0
+
+//timer starts when start button clicked 
+function startGame() {
     timerEl.textContent=time;
+    setInterval(countdown, 1000);
+    startGameTextEl.setAttribute("class", "hide");
+    startQuestions();
 }
-startButtonEl.addEventListener("click", startTimer);
-// setInterval(startGame, 60000)
+
+function countdown() {
+    timerEl.textContent=time;
+    if (time>0) {
+        time--
+    };
+}
+
+function startQuestions() {
+    questionTextEl.textContent=questions[questionIndex].question;
+    var choices = answerChoicesEl.textContent=questions[questionIndex].answers;
+    console.log(choices.forEach((choice)=>{
+        console.log(choice)
+    }));
+}
+
+startButtonEl.addEventListener("click", startGame);
+
+// when user clicks a button, make questionIndex ++ --function for that 
+//create element button and assign to each answer 
+//for each loop !! look up 
+//create a button and for each button set equal to the choice 
+// use hide class 
+// removeAttribute? 
 
 
-
+//question data 
 const questions = [
     {
-        id: "1",
         question: "Which operator means 'equal in value and type?",
         answers: ["=", "==", "===", "=&="], 
         correctAnswerIndex: 3
     },
     {
-        id: "2",
         question: "Which type of variable is allowed to be redeclared?",
         answers: ["var", "let", "const", "all of the above"], 
         correctAnswerIndex: 1
     },
     {
-        id: "3",
         question: "What type of brackets are used to enclose an array?",
         answers: ["curly brackets", "straight brackets", "quotes", "parenthesis"], 
         correctAnswerIndex: 2
     },
     {
-        id: "4",
         question: "Which math method rounds a number down to its nearest integer?",
         answers: ["Math.round(x)", "Math.ceil(x)", "Math.trunc(x)", "Math.floor(x)"], 
         correctAnswerIndex: 4
     },
     {
-        id: "5",
         question: "Which of these is not a common data type? ",
         answers: ["boolean", "word", "string", "number"], 
         correctAnswerIndex: 2
     }
   ]; 
 
-//                      
 
-// const question = questions.map(
-//     function(q) {
-//         document.getElementById("question-text").innerText = q.question
-//         console.log(q.answers[0])
-//         // const answer = q.answers.map(
-//         //     function(a) {
-//         //         // console.log(a)
-//         //         document.createElement("button").className = "answer-button"
-//         //         document.createElement("h3").innerHTML = a
-//         //     }
-//         // )
-//     });
-
-// var titleText = document.getElementById()
 
 // create somewhere on html a div that can hold the questions
 // somewhere for buttons for answer choices in html 
