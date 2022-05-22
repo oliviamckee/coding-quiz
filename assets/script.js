5c0c6498740eb829eec8ac98ad2cf297e6b2d73f
Olivia McKee <olivia.mckee97@gmail.com>
2022-05-21 19:20:04 -0500

    quiz functionality

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,114 @@
+//question data 
+var questions = [
+    {
+        question: "Which operator means 'equal in value and type?",
+        answers: ["a. =", "b. ==", "c. ===", "d. =&="], 
+        correctAnswerIndex: 2
+    },
+    {
+        question: "Which type of variable is allowed to be redeclared?",
+        answers: ["a. var", "b. let", "c. const", "d. all of the above"], 
+        correctAnswerIndex: 0
+    },
+    {
+        question: "What type of brackets are used to enclose an array?",
+        answers: ["a. curly brackets", "b. straight brackets", "c. quotes", "d. parenthesis"], 
+        correctAnswerIndex: 1
+    },
+    {
+        question: "Which math method rounds a number down to its nearest integer?",
+        answers: ["a. Math.round(x)", "b. Math.ceil(x)", "c. Math.trunc(x)", "d. Math.floor(x)"], 
+        correctAnswerIndex: 3
+    },
+    {
+        question: "Which of these is not a common data type? ",
+        answers: ["a. boolean", "b. word", "c. string", "d. number"], 
+        correctAnswerIndex: 1
+    }
+  ]; 
 var startButtonEl = document.getElementById("start-button");
 var timerEl = document.getElementById("timer");
-var time = 10;
+var time = 60;
 var questionsEl = document.getElementById("question");
 var startGameTextEl = document.getElementById("start-game-text");
 var answerChoicesEl = document.querySelector(".answer-choices");
 var questionTextEl = document.getElementById("question-text");
 var questionIndex = 0
+var endGameTextEl = document.querySelector(".end-game");
+var finalScoreEl = document.getElementById("final-score");
+var stopTimer = false;
+var submitScore = document.getElementById("submit-score");
 
 //timer starts when start button clicked 
 function startGame() {
     timerEl.textContent=time;
     setInterval(countdown, 1000);
     startGameTextEl.setAttribute("class", "hide");
+    startButtonEl.setAttribute("class", "hide");
     startQuestions();
 }
 
 function countdown() {
     timerEl.textContent=time;
+    if (stopTimer) {
+        return;
+    }
     if (time>0) {
         time--
     };
-}
-
-function startQuestions() {
-    questionTextEl.textContent=questions[questionIndex].question;
-    var choices = answerChoicesEl.textContent=questions[questionIndex].answers;
-    console.log(choices.forEach((choice)=>{
-        console.log(choice)
-    }));
+    if (time === 0) {
+        endGame();
+    };
+    
 }
 
 startButtonEl.addEventListener("click", startGame);
 
-// when user clicks a button, make questionIndex ++ --function for that 
-//create element button and assign to each answer 
-//for each loop !! look up 
-//create a button and for each button set equal to the choice 
-// use hide class 
-// removeAttribute? 
-
-
-//question data 
-const questions = [
-    {
-        question: "Which operator means 'equal in value and type?",
-        answers: ["=", "==", "===", "=&="], 
-        correctAnswerIndex: 3
-    },
-    {
-        question: "Which type of variable is allowed to be redeclared?",
-        answers: ["var", "let", "const", "all of the above"], 
-        correctAnswerIndex: 1
-    },
-    {
-        question: "What type of brackets are used to enclose an array?",
-        answers: ["curly brackets", "straight brackets", "quotes", "parenthesis"], 
-        correctAnswerIndex: 2
-    },
-    {
-        question: "Which math method rounds a number down to its nearest integer?",
-        answers: ["Math.round(x)", "Math.ceil(x)", "Math.trunc(x)", "Math.floor(x)"], 
-        correctAnswerIndex: 4
-    },
-    {
-        question: "Which of these is not a common data type? ",
-        answers: ["boolean", "word", "string", "number"], 
-        correctAnswerIndex: 2
+//start game
+function startQuestions() {
+    questionTextEl.textContent=questions[questionIndex].question;
+    var choices = questions[questionIndex].answers;
+    while (answerChoicesEl.lastChild) {
+        answerChoicesEl.removeChild(answerChoicesEl.lastChild)
     }
-  ]; 
+    choices.forEach((choice)=>{
+        var answerButtonEl = document.createElement("button");
+        answerButtonEl.textContent=choice;
+        answerChoicesEl.appendChild(answerButtonEl);
+        answerButtonEl.addEventListener("click", answerQuestion)
+   
+    });
+}
+
+//handle answer input
+function answerQuestion(event) {
+    // console.log(event.target.textContent);
+    var correctAnswer = (event.target.textContent === questions[questionIndex].answers[questions[questionIndex].correctAnswerIndex])
+    if (!correctAnswer) {
+        time = time - 10;
+    }
+    questionIndex++;
+    if (questionIndex < questions.length) startQuestions();
+    else endGame();
+}
+
+//end game 
+function endGame() {
+    stopTimer = true;
+    console.log(stopTimer);
+    questionTextEl.setAttribute("class", "hide");
+    answerChoicesEl.setAttribute("class", "hide");
+    endGameTextEl.classList.remove("hide"); 
+    finalScoreEl.textContent=time; 
+}
+
+function highScores() {
+    localStorage.setItem("high-score", time)
+}
 
 
-
-// create somewhere on html a div that can hold the questions
-// somewhere for buttons for answer choices in html 
-// timerEl **logic to go here later 
-// start button 
 // store high score to local storage w initials 
-// end game div -- All done your final score is... 
+
+
+
+
